70fa6f43ac78d562202fd391817f02e3111afd02
SonikaSahana <deoghariasonika@gmail.com>
2024-03-17 12:11:05 +0530

    login_page

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 const bodyParser = require("body-parser");
@@ -7,8 +8,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //Router
-const userRouter = require("./router/userRouter");
+// const userRouter = require("./router/userRouter");
 
-app.use("/", userRouter);
+// app.use("/", userRouter);
+app.get('/', (req, res) => {
+    // Serve your HTML file
+    res.sendFile(path.join(__dirname, "frontend", "views", "login.html"));
+  });
 
 app.listen(3000);
