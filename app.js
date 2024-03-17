89e7158cd27f0696b8af74591ec7612aee1a2a09
SonikaSahana <deoghariasonika@gmail.com>
2024-03-17 13:29:00 +0530

    backend_signup

--- app.js
+++ app.js
@@ -7,13 +7,27 @@ app.use(express.static("frontend"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const dotenv = require("dotenv");
+dotenv.config();
+
+const sequelize = require("./util/database");
+
 //Router
-// const userRouter = require("./router/userRouter");
+const userRouter = require("./router/userRouter");
 
-// app.use("/", userRouter);
-app.get('/', (req, res) => {
-    // Serve your HTML file
-    res.sendFile(path.join(__dirname, "frontend", "views", "login.html"));
-  });
+const User = require("./models/user");
 
-app.listen(3000);
+ app.use("/", userRouter);
+ app.use("/user/signup",userRouter)
+// app.get('/', (req, res) => {
+//     // Serve your HTML file
+//     res.sendFile(path.join(__dirname, "frontend", "views", "login.html"));
+//   });
+
+sequelize
+.sync()
+  //.sync({ force: true })
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
--- controller/userController.js
+++ controller/userController.js
@@ -1,3 +1,8 @@
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const sequelize = require("../util/database");
+const { Op } = require("sequelize");
+
 const path = require("path");
 const getLoginPage = async (req, res, next) => {
     try {
@@ -7,7 +12,47 @@ const getLoginPage = async (req, res, next) => {
     }
   };
 
+  const userSignUp = async (req, res, next) => {
+    try {
+      const name = req.body.name;
+      const email = req.body.email;
+      const number = req.body.number;
+      const password = req.body.password;
+  
+      const existingUser = await User.findOne({
+        where: {
+          [Op.or]: [{ email }, { number }],
+        },
+      });
+  
+      if (existingUser) {
+        res
+          .status(409)
+          .send(
+            `<script>alert('This email or number is already taken. Please choose another one.'); window.location.href='/'</script>`
+          );
+      } else {
+        bcrypt.hash(password, 10, async (err, hash) => {
+          await User.create({
+            name: name,
+            email: email,
+            number: number,
+            password: hash,
+          });
+        });
+        res
+          .status(200)
+          .send(
+            `<script>alert('User Created Successfully!'); window.location.href='/'</script>`
+          );
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   module.exports = {
     getLoginPage,
+    userSignUp
   };
   
